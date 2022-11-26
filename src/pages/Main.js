c607c4fa0303faf9c58b45c965f019c8151ba5d5
epicX67 <souravgain605@gmail.com>
2022-11-26 13:07:52 +0530

    Added styling on main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import heroLogo from "../res/heroLogo.png";
+import "./main.scss";
 
 export default function Main() {
   const profile = useSelector((store) => store.profile);
@@ -14,12 +16,33 @@ export default function Main() {
   }, []);
 
   return (
-    <div>
-      <h1>Next Invoice</h1>
+    <div className="rootHero">
+      <div className="heroHeader">
+        <img src={heroLogo} alt="next invoice" />
+        <p>
+          Next <span>Invoice</span>
+        </p>
+      </div>
+      <div className="heroSubHeader">
+        An effective way to create invoices for free
+      </div>
+
+      <div className="heroActions">
+        <Link className="anchorButton login" to="/login">
+          <i class="ri-login-circle-line"></i>
+          Login
+        </Link>
+        <Link className="anchorButton signup" to="/create">
+          <i class="ri-user-add-line"></i>
+          Create Profile
+        </Link>
+      </div>
+
+      {/* <h1>Next Invoice</h1>
       <p>Create invoice for free</p>
       <Link to="/login">Login</Link>
       <br></br>
-      <Link to="/create">Create Profile</Link>
+      <Link to="/create">Create Profile</Link> */}
     </div>
   );
 }
