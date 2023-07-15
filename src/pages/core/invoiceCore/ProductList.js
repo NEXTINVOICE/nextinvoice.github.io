278634901c20197dde3b8ec30b76df0d5d3f181b
epicX67 <souravgain605@gmail.com>
2023-07-15 21:20:16 +0530

    added product list table

--- src/pages/core/invoiceCore/ProductList.js
+++ src/pages/core/invoiceCore/ProductList.js
@@ -12,6 +12,55 @@ export default function ProductList({
 
   return (
     <>
+      {products.length > 0 && (
+        <table className="productListTable">
+            <tr className="productHeader">
+              <th>S no</th>
+              <th>Item Name</th>
+              <th>HSN</th>
+              <th>QTY</th>
+              <th>GST RATE</th>
+              <th>GST Amount</th>
+              <th>Cost</th>
+              <th>Total Amount</th>
+              <th>Actions</th>
+            </tr>
+
+            {products.map((item, index) => (
+              <tr key={index} className="productItem">
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+                <td>{isNaN(item.hsn) ? "N/A" : item.hsn}</td>
+                <td>{item.qty}</td>
+                <td>{item.gst}</td>
+                <td>{item.gstAmount}</td>
+                <td>{item.amount}</td>
+                <td>{item.total}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      setProductIndex(index);
+                      showEditProductModal(true);
+                    }}
+                    title="edit"
+                  >
+                    <i className="ri-edit-box-line"></i>
+                  </button>
+                  <button
+                    onClick={() => {
+                      setProductIndex(index);
+                      showDeleteProductModal(true);
+                    }}
+                    title="delete"
+                  >
+                    <i className="ri-delete-bin-line"></i>
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </table>
+      )}
+
       <AddProduct />
     </>
   );
